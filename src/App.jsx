8b3cf189684622074fe72109141e9572a6cb2cf1
mateusjairan <mateusjairan@gmail.com>
2024-07-15 16:59:49 -0300

    modificação do react

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg'; // Corrigido o caminho do import
+import './App.css';
+import Header from './assets/componentes/Header'; // Corrigido o nome do componente
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  let gato = {
+    nome: "Cleitinho",
+    profissao: "Pet da casa"
+  };
 
   return (
     <>
+      <Header titulo="Titulo" subTitulo="Titulo 2" />
+      <Header titulo="Primeiro Parâmetro" subTitulo="Segundo Parametro" />
+      <Header titulo={gato.nome} subTitulo={gato.profissao} />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -29,7 +37,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/componentes/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./componentes";
+
+function App() {
+    return (
+        <div className="App">
+            <Header titulo="Título Principal" subTitulo="Subtítulo Secundário" />
+        </div>
+    );
+}
+
+export default App;
